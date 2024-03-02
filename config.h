036fb9135f1aa6c983742deaa37d8baf5adc1df3
Aaron <unrealapex@proton.me>
2024-03-02 13:52:03 -0800

    feat: add message patch

--- config.h
+++ config.h
@@ -11,5 +11,15 @@ static const char *colorname[NUMCOLS] = {
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
 
+
+/* default message */
+static const char * message = "type password to unlock";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "6x10";
+
 /* allow control key to trigger fail on clear */
 static const int controlkeyclear = 1;
